0c32875d19f8417e4843977b3491bfd9030b67b7
Bezdicekz <zdenek.bezdicek@gmail.com>
2024-09-13 11:36:19 +0200

    index.html - odkazy a JavaScript.html

--- script.js
+++ script.js
@@ -32,8 +32,63 @@ function vypisJmeno() {
     console.log("Zdeňku");
 }
 
+function nakrajejZeleninu(zelenina) {
+    console.log(zelenina);
+}
+
+// pou6it9 vztvo5en9 funkce s parametrem
+nakrajejZeleninu("mrkev");
+nakrajejZeleninu("cibuli");
+nakrajejZeleninu("rajčata");
+
+
 vypisCislo(5,10);
 vypisCislo(10,20);
 vypisCislo(20,30);
 vypisCislo(55,106);
 vypisJmeno();
+
+// Sčítací funkce
+function secti(a, b) {
+    console.log(a + b);
+}
+
+secti(1, 2); // 3
+secti(10, 20); // 30
+secti(100, 200); // 300
+
+var soucet = secti(100, 200); // 300
+
+console.log(soucet)
+
+// pole
+let pole = [1, 2, 3, 4, 5, 6, 7];
+let pole1 = ["a", "b", true, ["ahoj","nazdar"]];
+
+console.log(pole1[2]);
+
+// Objekt
+let osoba = {
+    jemno: "Tomáš",
+    vek: 25,
+    pravda: true,
+    kolegove: ["Tomáš", "Karel", "Marie"],
+    zamestnavatel: {
+        holding: {
+            nazev: "PPF Group",
+            adresa: "Praha1",
+            pocetfirem: 10,
+        },
+        nazev: "Google",
+        adresa: "Praha6",
+        pocetZamestnancu: 1000,
+    },
+};
+
+console.log(osoba.jemno)
+console.log(osoba.zamestnavatel.holding.nazev)
+
+osoba.jemno = "Petr";
+
+console.log(osoba.jemno)
+
